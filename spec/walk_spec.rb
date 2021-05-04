dd653835d2939e2a40b70d4baa207f665b56c114
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-04 17:43:28 +0100

    test for non equal west and east directions passes

--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -10,6 +10,9 @@ require 'walk'
     it 'returns true if there are equal numbers of east and west directions' do
       expect(ten_minute_walk?(['w', 'w','w','w','w','e','e','e','e','e'])).to eq(true)
     end
+    it 'returns true if there are non equal numbers of east and west directions' do
+      expect(ten_minute_walk?(['w', 'w','w','w','w','w','e','e','e','e'])).to eq(false)
+    end
     # it 'returns false if the user doesnt reach original location' do
     #   expect(ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq(false)
     # end
