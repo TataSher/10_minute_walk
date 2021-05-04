4a41d370f983b2a5a16a5a29e8bf8591b504f1b2
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-04 17:29:39 +0100

    test for equal west and east directions passes

--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -4,7 +4,13 @@ require 'walk'
     it 'returns true if there are 10 directions given' do
       expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq(true)
     end
-    it 'returns false if the user doesnt reach original location' do
-      expect(ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq(false)
+    it 'returns false if there are less or more than 10 directions given' do
+      expect(ten_minute_walk?(['w', 's'])).to eq(false)
     end
+    it 'returns true if there are equal numbers of east and west directions' do
+      expect(ten_minute_walk?(['w', 'w','w','w','w','e','e','e','e','e'])).to eq(true)
+    end
+    # it 'returns false if the user doesnt reach original location' do
+    #   expect(ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq(false)
+    # end
 end
